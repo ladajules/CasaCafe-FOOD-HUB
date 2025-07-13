1e43ee8eb01d9039e3e4d376fded663e7b8b8ef6
Jules Lada <juleslada35@gmail.com>
2025-07-13 23:07:13 +0800

    Created another function to fetch users.

---
+++ admin_get_users.php
@@ -0,0 +1,17 @@
+<?php
+require 'db_connection.php'; 
+
+$sql = "SELECT UserID, Username FROM login ORDER BY UserID DESC";
+$result = $conn->query($sql);
+
+$users = [];
+
+if ($result && $result->num_rows > 0) {
+  while ($row = $result->fetch_assoc()) {
+    $users[] = $row;
+  }
+}
+
+header('Content-Type: application/json');
+echo json_encode($users);
+?>
