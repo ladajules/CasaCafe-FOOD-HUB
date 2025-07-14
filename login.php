c47efc011a8722ea90f0ac1ba581d1b7390cc454
Jules Lada <juleslada35@gmail.com>
2025-07-14 11:13:43 +0800

    "Trying capitalized username"

--- login.php
+++ login.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
     //if admin then redirect to dashboard admin_dashboard.html
-    if ($user === "casacafe_admin" && password_verify($password, "123")) {
+    if ($user === "Casacafe_admin" && password_verify($password, "123")) {
         header("Location: admin_users.html");
         exit;
     } else {
