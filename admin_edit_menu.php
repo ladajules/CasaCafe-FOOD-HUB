a2782d49eb8db5c45ed1c861a5757d7090898cd1
Jules Lada <juleslada35@gmail.com>
2025-07-15 21:34:36 +0800

    "Updated view menu and added edit menu php"

---
+++ admin_edit_menu.php
@@ -0,0 +1,31 @@
+<?php
+require 'db_connection.php';
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if (
+  isset($data['item_id'], $data['item_name'], $data['item_category'], 
+        $data['item_description'], $data['item_price'], $data['item_image'])
+) {
+  $stmt = $conn->prepare("UPDATE menu SET item_name=?, item_category=?, item_description=?, item_price=?, item_image=? WHERE item_id=?");
+  $stmt->bind_param(
+    "ssssdi",
+    $data['item_name'],
+    $data['item_category'],
+    $data['item_description'],
+    $data['item_price'],
+    $data['item_image'],
+    $data['item_id']
+  );
+
+  if ($stmt->execute()) {
+    echo json_encode(["success" => true]);
+  } else {
+    echo json_encode(["success" => false, "error" => $stmt->error]);
+  }
+
+  $stmt->close();
+} else {
+  echo json_encode(["success" => false, "error" => "Missing fields"]);
+}
+?>
