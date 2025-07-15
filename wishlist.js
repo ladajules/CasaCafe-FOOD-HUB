eda94291c80e2c87b43ec3bad53f4fa592948e4a
TheSchneide <franciscojubelagxx@gmail.com>
2025-07-16 00:06:59 +0800

    "ang dropdown sa wishlist"

--- wishlist.js
+++ wishlist.js
@@ -124,3 +124,22 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   }
 });
+
+
+
+const toggle = document.getElementById("dropdownToggle");
+  const menu = document.getElementById("dropdownMenu");
+  const arrow = document.getElementById("dropdownArrow");
+
+  toggle.addEventListener("click", () => {
+    const isVisible = menu.style.display === "block";
+    menu.style.display = isVisible ? "none" : "block";
+    toggle.classList.toggle("open", !isVisible);
+  });
+
+  window.addEventListener("click", (e) => {
+    if (!document.getElementById("profileDropdown").contains(e.target)) {
+      menu.style.display = "none";
+      toggle.classList.remove("open");
+    }
+  });
