7c1298f8c7d609e0e44869d16415121a18de730b
Jules Lada <juleslada35@gmail.com>
2025-07-15 23:06:56 +0800

    "Fixing save button"

--- admin_edit_menu.php
+++ admin_edit_menu.php
@@ -1,15 +1,16 @@
 <?php
-require 'db_connection.php';
+require 'db_connection.php'; 
 
 $data = json_decode(file_get_contents('php://input'), true);
+error_log("Incoming JSON: " . file_get_contents('php://input'));
 
 if (
-  isset($data['item_id'], $data['item_name'], $data['item_category'], 
+  isset($data['item_id'], $data['item_name'], $data['item_category'],
         $data['item_description'], $data['item_price'], $data['item_image'])
 ) {
-  $stmt = $conn->prepare("UPDATE menu SET item_name=?, item_category=?, item_description=?, item_price=?, item_image=? WHERE item_id=?");
+  $stmt = $conn->prepare("UPDATE menu SET item_name = ?, item_category = ?, item_description = ?, item_price = ?, item_image = ? WHERE item_id = ?");
   $stmt->bind_param(
-    "ssssdi",
+    "sssisi",
     $data['item_name'],
     $data['item_category'],
     $data['item_description'],
