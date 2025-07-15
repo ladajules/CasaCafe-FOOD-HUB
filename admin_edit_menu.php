6b04dd15a13e118d049deba7f795e55802e229e8
Jules Lada <juleslada35@gmail.com>
2025-07-15 23:28:58 +0800

    "Updated"

--- admin_edit_menu.php
+++ admin_edit_menu.php
@@ -4,16 +4,11 @@ require 'db_connection.php';
 $data = json_decode(file_get_contents('php://input'), true);
 
 if (
-  is_array($data) &&
-  array_key_exists('item_id', $data) &&
-  array_key_exists('item_name', $data) &&
-  array_key_exists('item_category', $data) &&
-  array_key_exists('item_description', $data) &&
-  array_key_exists('item_price', $data) &&
-  array_key_exists('item_image', $data)
+    isset($data['item_id'], $data['item_name'], $data['item_category'],
+    $data['item_description'], $data['item_price'], $data['item_image'])
 ) {
   $stmt = $conn->prepare("UPDATE menu SET item_name = ?, item_category = ?, item_description = ?, item_price = ?, item_image = ? WHERE item_id = ?");
-  $stmt->bind_param("sssisi",
+  $stmt->bind_param("isssds",
     $data['item_id'],
     $data['item_name'],
     $data['item_category'],
