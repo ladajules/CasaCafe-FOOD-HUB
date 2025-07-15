14b4e83d5b7a195328eaa80e5ec727ae2b1e4973
Cisco <franciscojubelagxx@gmail.com>
2025-07-15 11:15:52 +0800

    "try error message"

--- login.php
+++ login.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
          }
          exit;
     } else {
-      header("Location: login.html?error=1");
+      $error_message = 'Incorrect username or password. Please try again.';
     }
 }
 ?>
