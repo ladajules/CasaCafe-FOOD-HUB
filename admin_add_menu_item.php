05132f1dfe2555225fde3547e7bdf0b24767ad21
Jules Lada <juleslada35@gmail.com>
2025-07-16 22:39:01 +0800

    "eeror checking"

--- admin_add_menu_item.php
+++ admin_add_menu_item.php
@@ -1,6 +1,8 @@
 <?php
 require 'db_connection.php';
 
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json');
 
 $input = json_decode(file_get_contents('php://input'), true);
