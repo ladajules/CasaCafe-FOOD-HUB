5388c586685fc6f321e38f8417ad5a89e5e0afac
Jules Lada <juleslada35@gmail.com>
2025-07-17 22:03:36 +0800

    "try"

--- edit_users.php
+++ edit_users.php
@@ -4,7 +4,7 @@ require 'db_connection.php';
 $newUsername = $_POST['username'] ?? '';
 $user_id = $_POST['user_id'] ?? '';
 
-if (!$username || !$user_id) {
+if (!$newUsername || !$user_id) {
     echo json_encode(['success' => false, 'error' => 'Missing data']);
     exit;
 }
