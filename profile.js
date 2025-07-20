eb0709e25b245288954b7c36debf31bfeb6f5751
Jules Lada <juleslada35@gmail.com>
2025-07-20 13:08:15 +0800

    'back to top button'

--- profile.js
+++ profile.js
@@ -303,3 +303,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+const backToTopBtn = document.getElementById("backToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.opacity = "1";
+    backToTopBtn.style.visibility = "visible";
+  } else {
+    backToTopBtn.style.opacity = "0";
+    backToTopBtn.style.visibility = "hidden";
+  }
+});
+
+backToTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
